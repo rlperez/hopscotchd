86b17f867931298d758a20018aed461a7ee4f32d
Rigoberto L. Perez <rlperez@udel.edu>
2020-06-30 09:03:09 -0400

    Remove old migrations in favor of schema gen output

--- priv/repo/migrations/20200625022956_create_brewery_type_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule Hopscotchd.Repo.Migrations.CreateBreweryTypeTable do
-  use Ecto.Migration
-
-  def change do
-    create table(:tags) do
-      add :type, :string, null: false, size: 32
-    end
-  end
-end
